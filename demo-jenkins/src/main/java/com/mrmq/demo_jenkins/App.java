4a897a2d10ffcf25ce9152298e31696a625ea188
quyenlm <quyenlm.vn@gmail.com>
2016-09-15 17:02:09 +0700

    update for 20160924

--- demo-jenkins/src/main/java/com/mrmq/demo_jenkins/App.java
+++ demo-jenkins/src/main/java/com/mrmq/demo_jenkins/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!2" );
+        System.out.println( "Hello World!20160924" );
     }
 }
